ea0fd3b25d417a1bd7198f2e00a1bd78df40ee04
stoneG <me@sitong>
2013-01-15 15:35:47 -0800

    cells now know how many mines neighbor it

--- sweeper.js
+++ sweeper.js
@@ -11,45 +11,67 @@ function Board(nRows, nCols, nMines) {
 
     this.init = function() {
         board.minesLeft = board.nMines;
-
+        var i, j;
         board.mines = function() {
             var mines = [], position;
-            for (var i = 0; i < board.nMines; i++) {
+            for (i = 0; i < board.nMines; i++) {
                 position = Math.floor(Math.random() * board.nCols * board.nRows);
-                if (mines[position])
+                if (mines[position]) {
                     i--;
-                else
+                } else {
                     mines[position] = true;
-            };
+                }
+            }
             return mines;
         }();
-
-        for (var i = 0; i < board.nRows; i++) {
-            board.cells[i] = []
-            for (var j = 0; j < board.nCols; j++) {
+        for (i = 0; i < board.nRows; i++) {
+            board.cells[i] = [];
+            for (j = 0; j < board.nCols; j++) {
                 isMine = board.mines[i * nRows + j] || false;
                 board.cells[i][j] = new Cell(i, j, isMine);
-                if (!isMine)
-                    board.cells[i][j].number = board.get_number(i, j)
-            };
-        };
+                if (!isMine) {
+                    board.cells[i][j].number = board.getNumber(i, j);
+                }
+            }
+        }
     };
 
-    this.get_number(row, col) {
-        // TODO calculate cell's number
+    this.getNeighbors = function(row, col) {
+        var neighbors = [],
+            i, j;
+        for (i = Math.max(row-1,0); i < Math.min(row+2, board.nRows); i++) {
+            for (j = Math.max(col-1,0); j < Math.min(col+2, board.nCols); j++) {
+                if ((i != row) || (j != col)) {
+                    neighbors.push(i * nRows + j);
+                }
+            }
+        }
+        return neighbors;
+    };
+
+    this.getNumber = function(row, col) {
+        var neighbors = this.getNeighbors(row, col),
+            number = 0;
+        for (var i = 0; i < neighbors.length; i++) {
+            if (board.mines[neighbors[i]]) {
+                number++;
+            }
+        }
+        return number;
     };
 }
 
 function Cell(row, col, isMine) {
-    var cell = this, number;
+    var cell = this,
+        number = 0;
     cell.row = row;
     cell.col = col;
     cell.isMine = isMine;
 
-    this.sweep = function() {
+    /*this.sweep = function() {
         if (cell.isMine)
             // TODO show bomb
         else
             // TODO show number
-    };
+    };*/
 }
